bd79e9ebb759dce0169a7e7b8d574b352312dabb
suri <surendra@gmail.com>
2023-11-28 12:10:15 +0530

    com

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
+const connectMongos = async () => {
+  try {
+    const mongooseConnectionURI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/curdoperation";
+    const res = await mongoose.connect(mongooseConnectionURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-const connectMongos=async()=>{
-try {
-    const res=await mongoose.connect("mongodb://127.0.0.1:27017/curdoperation");
-
-        console.log("connection  sucessfully");
-    
-} catch (error) {
-    console.log("check",error);
-}
-
+    console.log("Connection successfully established");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 };
-export default connectMongos;
 
+export default connectMongos;
